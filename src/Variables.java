3bca9fd43399a6aebb04dfa34cbb53074d585774
nhung14042001 <nhung14042001@gmail.com>
2025-02-10 18:43:53 +0700

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -10,9 +10,9 @@ String nhungntt = "helloword";
         long numberLong = 7887654345456789067L;
         float numberFloat = 12.34F;
         double numberDouble = 12.34567D;
-int ex =100;
-int q,w,e,r;
-q=w=e=r=ex;
+// int ex =100;
+// int q,w,e,r;
+// q=w=e=r=ex;
         System.out.println(q&w&e&r);
 }
 }
