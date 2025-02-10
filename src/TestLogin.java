766508d6d445762014765ee18d79fd233464d143
nhung14042001 <nhung14042001@gmail.com>
2025-02-10 19:43:29 +0700

    Update TestLogin.java

--- src/TestLogin.java
+++ src/TestLogin.java
@@ -1,6 +1,6 @@
 public class TestLogin {
     public static void main(String[] args) {
         System.out.println("Hello word");
-        System.out.println("Ahihi");
+        // System.out.println("Ahihi");
     }
 }
