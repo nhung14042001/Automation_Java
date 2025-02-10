588bd4692fce70932939762a80d6ffd899d08c61
nhung14042001 <nhung14042001@gmail.com>
2025-02-10 18:59:55 +0700

    Update Mang_1.java
    Test demo

--- src/Arrays/Mang_1.java
+++ src/Arrays/Mang_1.java
@@ -5,12 +5,12 @@ public class Mang_1 {
         //khai báo mảng rỗng, chưa mang giá trị
         int number1 []= new int[6];
         // Gán giá trị cho mảng
-        number1[0]=40;
-        number1[1]=20;
-        number1[2]=10;
-        number1[3]=50;
-        number1[4]=5;
-        number1[5]=10;
+//        number1[0]=40;
+//        number1[1]=20;
+//        number1[2]=10;
+//        number1[3]=50;
+//        number1[4]=5;
+//        number1[5]=10;
         System.out.println("Độ dài mảng = " + number1.length);
         System.out.println("===================");
         System.out.println(number1[3]);
